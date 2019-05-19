a8a4239c3969d298e05c83af47111fda7dd3f468
Justin Endler <justin.endler@brandingbrand.com>
2019-05-19 15:32:31 -0400

    feat: Add more App component tests

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Switch, Route } from 'react-router-dom';
 import { shallow } from 'enzyme';
 
 import App from 'components/App';
@@ -13,4 +14,12 @@ beforeEach(() => {
 
 it('shows Nav component', () => {
   expect(app.find(Nav).length).toEqual(1);
+});
+
+it('shows Switch component', () => {
+  expect(app.find(Switch).length).toEqual(1);
+});
+
+it('shows Route component', () => {
+  expect(app.find(Route).length).toBeGreaterThanOrEqual(1);
 });
